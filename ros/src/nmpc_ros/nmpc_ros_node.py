7c1ae705e9ff97a62064e33a0b348c9317f7a9d4
andrearlotta <aarlotta@os.uniroma3.it>
2025-02-26 13:10:02 +0100

    minor change

--- ros/src/nmpc_ros/nmpc_ros_node.py
+++ ros/src/nmpc_ros/nmpc_ros_node.py
@@ -3,4 +3,4 @@ from nmpc_ros_package.nmpc  import run_simulation, plot_animated_trajectory_and_
 if __name__ == '__main__':
     # Run the simulation with the default parameters
     all_trajectories, entropy_history, lambda_history, durations, g_nn, trees_pos, lb, ub = run_simulation()
-    entropy_mpc_pred = plot_animated_trajectory_and_entropy_2d(g_nn,all_trajectories, entropy_history, lambda_history, trees_pos, lb, ub, durations)
+    entropy_mpc_pred = plot_animated_trajectory_and_entropy_2d(all_trajectories, entropy_history, lambda_history, trees_pos, lb, ub, durations)
