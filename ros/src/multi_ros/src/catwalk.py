e95764c17e7986ba59a1b076bc8ffa95ce79a67f
Andrea <andreamiele120@gmail.com>
2025-03-29 18:13:08 +0100

    test

--- ros/src/multi_ros/src/catwalk.py
+++ ros/src/multi_ros/src/catwalk.py
@@ -20,9 +20,9 @@ def main():
     y = (-15.0, -10.0, -6.0)
     z = -0.6
 
-    pub_drone_0 = rospy.Publisher('drone_0/cmd/pose', Pose, queue_size=10)
-    pub_drone_1 = rospy.Publisher('drone_1/cmd/pose', Pose, queue_size=10)
-    pub_drone_2 = rospy.Publisher('drone_2/cmd/pose', Pose, queue_size=10)
+    pub_drone_0 = rospy.Publisher('agent_1/cmd/pose', Pose, queue_size=10)
+    pub_drone_1 = rospy.Publisher('agent_2/cmd/pose', Pose, queue_size=10)
+    pub_drone_2 = rospy.Publisher('agent_3/cmd/pose', Pose, queue_size=10)
 
     initial_pose_0 = create_pose(0.0, y[0], z)   
     initial_pose_1 = create_pose(0.0, y[1], z)   
